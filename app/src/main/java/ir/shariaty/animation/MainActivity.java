b57c42270f621d54a6741a7f78428bb3db54e693
Hossein <djalicrt@gmail.com>
2021-03-08 18:29:04 +0330

    First Commit

---
+++ app/src/main/java/ir/shariaty/animation/MainActivity.java
@@ -0,0 +1,29 @@
+package ir.shariaty.animation;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class MainActivity extends AppCompatActivity {
+
+    LottieAnimationView button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                button.playAnimation();
+            }
+        });
+    }
+}
